a2cc628cfef98968221f40d3fe1b38a083da9a09
LZ <lizong20001204@gmail.com>
2022-07-22 10:19:04 +0800

    feature: add the main menu

--- src/components/writingRoom.vue
+++ src/components/writingRoom.vue
@@ -2,15 +2,26 @@
   <div>
     <n-grid x-gap="6" :cols="12">
       <n-gi span="2">
+
         <n-menu
-            v-model:value="activeFile"
+            v-model:value="activateTheTag"
+            :indent="12"
+            :options="tags"
+            @click="activateTheFile = null"
+        />
+
+        <n-divider/>
+
+        <n-menu
+            v-model:value="activateTheFile"
             :indent="12"
             :options="files"
+            @click="activateTheTag = null"
         />
       </n-gi>
       <n-gi span="10">
         <div>
-          <div id="article" class="preview"></div>
+          <div id="article" class="preview" v-if="vditorShow"></div>
         </div>
       </n-gi>
     </n-grid>
@@ -18,30 +29,51 @@
 </template>
 
 <script>
-import {NMenu, NGi, NGrid} from 'naive-ui'
+import {h} from 'vue'
+import {NMenu, NGi, NGrid, NDivider, NIcon} from 'naive-ui'
+import {Library, Settings} from '@vicons/ionicons5';
 import Vditor from 'vditor';
 import 'vditor/dist/index.css';
+function renderIcon(icon) {
+  return () => h(NIcon, null, { default: () => h(icon) });
+}
 
 export default {
   name: 'writingRoom',
   components: {
     NMenu,
     NGi,
-    NGrid
+    NGrid,
+    NDivider,
+    NIcon,
   },
   data() {
     return {
-      activeFile: '',
+      activateTheFile: null,
+      activateTheTag: null,
+      vditorShow: false,
       files: [
-
+        {
+          label: 'testVal',
+          key: 'testVal'
+        }
+      ],
+      tags: [
+        {
+          label: 'Store',
+          key: 'Store',
+          icon: renderIcon(Library)
+        },
+        {
+          label: 'Settings',
+          key: 'Settings',
+          icon: renderIcon(Settings)
+        },
       ]
     }
   },
-  mounted() {
-    this.init();
-  },
   methods: {
-    init() {
+    initVditor() {
       this.content = new Vditor('article', {
         minHeight: 500,
         toolbar: [],
@@ -56,8 +88,9 @@ export default {
             }
           }
         }
-      })
-    }
+      });
+      this.vditorShow = true;
+    },
   }
 }
 </script>
