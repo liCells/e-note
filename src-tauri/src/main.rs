1d32900d15a18496ed2e0347cb05e2a2aa644c78
LZ <lizong20001204@gmail.com>
2022-07-23 11:47:52 +0800

    feature: add table of store

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,31 @@
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
+extern crate r2d2;
+extern crate r2d2_sqlite;
+extern crate rusqlite;
+
+use r2d2_sqlite::SqliteConnectionManager;
+use r2d2::Pool;
+use rusqlite::params;
 
 fn main() {
+  let db_pool = get_sqlite_pool();
+  db_pool.get()
+      .unwrap()
+      .execute("CREATE TABLE IF NOT EXISTS store (
+                  id INT PRIMARY KEY NOT NULL,
+                  content TEXT NOT NULL,
+                  tags TEXT
+               );", params![])
+      .unwrap();
+
   tauri::Builder::default()
     .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    .expect("error while running application");
+}
+
+fn get_sqlite_pool() -> Pool<SqliteConnectionManager> {
+  let manager = SqliteConnectionManager::file("mnote.db");
+  r2d2::Pool::new(manager).unwrap()
 }
