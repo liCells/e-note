fc3a3b3f0f291f12912489c49ed7d403b9e64ded
LZ <lizong20001204@gmail.com>
2022-07-24 12:03:18 +0800

    feature: add global connection pool and adjust init database

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,9 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
+#[macro_use]
+extern crate lazy_static;
 extern crate r2d2;
 extern crate r2d2_sqlite;
 extern crate rusqlite;
@@ -10,23 +12,30 @@ use r2d2_sqlite::SqliteConnectionManager;
 use r2d2::Pool;
 use rusqlite::params;
 
+// Global connection manager pool
+lazy_static! {
+    static ref DB_POOL: Pool<SqliteConnectionManager> =
+      r2d2::Pool::new(
+        SqliteConnectionManager::file("mnote.db")
+      ).unwrap();
+}
+
 fn main() {
-  let db_pool = get_sqlite_pool();
-  db_pool.get()
-      .unwrap()
-      .execute("CREATE TABLE IF NOT EXISTS store (
+    init();
+
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running application");
+}
+
+fn init() {
+    // init database
+    let _ = DB_POOL.get()
+        .unwrap()
+        .execute("CREATE TABLE IF NOT EXISTS store (
                   id INTEGER PRIMARY KEY AUTOINCREMENT,
                   content TEXT NOT NULL,
                   tags TEXT
                );", params![])
-      .unwrap();
-
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running application");
-}
-
-fn get_sqlite_pool() -> Pool<SqliteConnectionManager> {
-  let manager = SqliteConnectionManager::file("mnote.db");
-  r2d2::Pool::new(manager).unwrap()
+        .unwrap();
 }
